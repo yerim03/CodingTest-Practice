a4123e3d14edef033b486d9328c95883d3f7eb16
나예림 <103301583+yerim03@users.noreply.github.com>
2024-08-20 00:08:22 +0900

    [Silver II] Title: DFS와 BFS, Time: 296 ms, Memory: 20752 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -4,15 +4,15 @@ import java.util.*;
 public class Main {
     public static int n, m, v;
     public static boolean[] visited;
-    public static int[][] graph;
+    public static ArrayList<Integer>[] graph;
     public static Queue<Integer> q = new LinkedList<>();
 
     public static void dfs(int v) {
         visited[v] = true;  //현재 정점 방문 처리
         System.out.print(v + " ");
 
-        for (int i = 1; i <= n; i++) {
-            if (graph[v][i] == 1 && !visited[i]) {  //방문하지 않은 노드일 경우
+        for (Integer i : graph[v]) {
+            if (!visited[i]) {
                 dfs(i);
             }
         }
@@ -27,8 +27,9 @@ public class Main {
             int x = q.poll();
             System.out.print(x + " ");
 
-            for (int i = 1; i <= n; i++) {
-                if (graph[x][i] == 1 && !visited[i]) {  //방문하지 않은 노드일 경우
+
+            for (Integer i : graph[x]) {
+                if (!visited[i]) {  //방문하지 않은 노드일 경우
                     q.offer(i);
                     visited[i] = true;
                 }
@@ -44,12 +45,22 @@ public class Main {
          m = Integer.parseInt(st.nextToken());  //간선의 개수
          v = Integer.parseInt(st.nextToken());  //탐색 시작 번호
 
-         graph = new int[n+1][n+1];
+         graph = new ArrayList[n + 1];
+
+         for (int i = 0; i <= n; i++) {
+             graph[i] = new ArrayList<>();
+         }
+
          for (int i = 0; i < m; i++) {
              StringTokenizer st1 = new StringTokenizer(br.readLine());
              int x = Integer.parseInt(st1.nextToken());
              int y = Integer.parseInt(st1.nextToken());
-             graph[x][y] = graph[y][x] = 1; //양방향
+             graph[x].add(y);
+             graph[y].add(x); //양방향
+         }
+
+         for (ArrayList<Integer> list : graph) {
+             Collections.sort(list);
          }
 
          visited = new boolean[n + 1];
