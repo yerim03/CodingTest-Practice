c8230d8deedf738e2947205bac70b2de59f52d7b
나예림 <103301583+yerim03@users.noreply.github.com>
2024-09-05 16:10:46 +0900

    [Bronze II] Title: 대표값2, Time: 116 ms, Memory: 16020 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int[] arr = new int[5];
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
+        }
+
+        Arrays.sort(arr);
+        bw.write(sum /5 + "\n" + arr[2]);
+        bw.flush();
+        bw.close();
+    }
+}
