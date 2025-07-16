11066a08ba6a22c2973b92dce2a3c073dd70d8aa
나예림 <103301583+yerim03@users.noreply.github.com>
2025-07-16 11:05:05 +0900

    [Silver I] Title: 트럭, Time: 260 ms, Memory: 20812 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.java
+++ 백준/Silver/13335. 트럭/트럭.java
@@ -7,11 +7,7 @@ import java.util.*;
 
 
 public class Main {
-    
-    static int[] distance;
-    static ArrayList<Integer> onBridgeIdx = new ArrayList<>(); //다리 위 트럭들의 번호
-    static int onBridgeW = 0;  //다리 위 트럭들의 무게 합
-     
+
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
 
@@ -19,48 +15,37 @@ public class Main {
         int w = sc.nextInt();   //다리의 길이
         int l = sc.nextInt();   //다리의 최대하중
 
-        int[] truck = new int[n];
-        distance = new int[n];    //각 트럭이 다리를 모두 건넜는지 길이를 나타내는 배열
+        Queue<Integer> truck = new LinkedList<>();
+        Queue<Integer> bridge = new LinkedList<>();
+
+        //truck 큐에 트럭의 무게 삽입
         for (int i = 0; i < n; i++) {
-            truck[i] = sc.nextInt();
+            truck.add(sc.nextInt());
         }
+
+        //bridge 큐에 w개 만큼 0 삽입 - 현재 다리에 올라와 있는 트럭의 위치
+        for (int i = 0; i < w; i++) {
+            bridge.add(0);
+        }
+
 
         int result = 0; //최단시간
-        int idx = 0;    //트럭 번호
+        int onBridgeW = 0;  //다리에 올라와 있는 트럭들 무게의 합
 
-        while(true) {
-            //모든 트럭이 다 건넌 경우 종료
-            if (idx == n) {
-                break;
-            }
+        //다리에 아무것도 존재하지 않을 때까지 수행
+        while (!bridge.isEmpty()) {
+            result++;
 
-            if (onBridgeW + truck[idx] <= l) {  //l보다 작거나 같으면 idx번 트럭도 다리로 이동
-                onBridgeW += truck[idx];
-                onBridgeIdx.add(idx);
-                move();
-                
-                idx++;
-            } else {    //l보다 크면 다리 위에 있는 트럭트면 한 칸씩 이동
-                for (int i = 0; i < onBridgeIdx.size(); i++) {
-                    distance[onBridgeIdx.get(i)]++;
+            onBridgeW -= bridge.poll(); //맨 앞의 트럭부터 한 대씩 다리에서 내려온다.
+            if(!truck.isEmpty()) {
+                if (onBridgeW + truck.peek() <= l) {    //최대하중과 같거나 작을 경우
+                    onBridgeW += truck.peek();
+                    bridge.add(truck.poll());   //다리에 트럭 올라감
+                } else {    //최대하중보다 클 경우
+                    bridge.add(0);  //bridge 큐에 0 삽입(트럭이 앞으로 한 칸씩 이동)
                 }
             }
-            result++;   //이동시간 증가
-
-            int frontTdx = onBridgeIdx.get(0); //다리의 맨 앞에 있는 트럭번호
-            if (distance[frontTdx] == w) {  //해당 트럭이 다리를 모두 건넌 경우
-                onBridgeIdx.remove(0);
-                onBridgeW -= truck[frontTdx];
-            }
-
         }
-        result += w - distance[n-1] + 1;//마지막 트럭이 건너오는 시간까지 더한다.
         System.out.println(result);
-    }
-
-    public static void move() {
-        for (int i = 0; i < onBridgeIdx.size(); i++) {
-            distance[onBridgeIdx.get(i)]++;
-        }
     }
 }
