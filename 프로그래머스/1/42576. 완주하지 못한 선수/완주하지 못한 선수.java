01f359a4d6abbca2181ab30faeb52977bdf446f0
나예림 <103301583+yerim03@users.noreply.github.com>
2025-10-22 14:59:14 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 77.77 ms, Memory: 97.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,14 +4,25 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         
-        Arrays.sort(participant);
-        Arrays.sort(completion);
-
-        for(int i = 0; i < participant.length - 1; i++) { 
-            if(!participant[i].equals(completion[i])) {
-                return participant[i];
+        HashMap<String, Integer> hm = new HashMap<>();
+        
+        //map에 참가자 추가
+        for(String p : participant) {
+            hm.put(p, hm.getOrDefault(p, 0) + 1);
+        }
+        
+        //map에서 완주한 선수 제거
+        for(String c : completion) {
+            hm.put(c, hm.get(c) - 1);
+        }
+        
+        for(String key : hm.keySet()) {
+            if(hm.get(key) != 0) {
+                answer = key;
+                break;
             }
         }
-        return participant[participant.length - 1];
+        
+        return answer;
     }
 }
