f2914b149c446df52c9784dde4cc6930078cbda2
나예림 <103301583+yerim03@users.noreply.github.com>
2026-04-19 10:28:32 +0900

    [Silver IV] Title: 제로, Time: 204 ms, Memory: 23832 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,22 +1,20 @@
 import java.io.*;
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Main {
-    public static Stack<Integer> stack = new Stack<>();
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        Deque<Integer> stack = new ArrayDeque<>();
         int k = Integer.parseInt(br.readLine());
-
         for (int i = 0; i < k; i++) {
-            int num = Integer.parseInt(br.readLine());
+            int x = Integer.parseInt(br.readLine());
 
-            if (num == 0) {
+            if (x == 0) {
                 stack.pop();
             } else {
-                stack.push(num);
+                stack.push(x);
             }
         }
 
@@ -24,8 +22,7 @@ public class Main {
         while (!stack.isEmpty()) {
             sum += stack.pop();
         }
-        bw.write(sum + "\n");
-        bw.flush();
-        bw.close();
+
+        System.out.println(sum);
     }
 }
