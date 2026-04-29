e860712ec2430b0d3f9d7a5766d1f58bfdf6c8e3
나예림 <103301583+yerim03@users.noreply.github.com>
2026-04-29 17:05:12 +0900

    [level 2] Title: 기능개발, Time: 0.42 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -3,25 +3,20 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         ArrayList<Integer> list = new ArrayList<>();
+        Queue<Integer> q = new LinkedList<>();
         
-        int[] days = new int[progresses.length];
         for(int i = 0; i < progresses.length; i++) {
-            double day = Math.ceil((double)(100 - progresses[i]) / (double)(speeds[i]));
-            days[i] = (int) day;
-        }
-        
-        int cnt = 1;
-        int start = 0;
-        for(int i = 1; i < days.length; i++) {
-            if(days[start] >= days[i]) {
-                cnt++;
-            } else {
-                list.add(cnt);
-                start = i;
-                cnt = 1;
+            int day = (int) Math.ceil((double)(100 - progresses[i]) / (double)(speeds[i]));
+            
+            if(!q.isEmpty() && q.peek() < day) {
+                list.add(q.size());
+                q.clear();
             }
+            q.offer(day);
         }
-        list.add(cnt);
+        if(!q.isEmpty()) {
+            list.add(q.size());
+        }
         
         int[] answer = new int[list.size()];
         for(int i = 0; i < list.size(); i++) {
