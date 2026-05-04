a3240d3d5cf66d5f6b62bc96d8b77cda36716c0e
나예림 <103301583+yerim03@users.noreply.github.com>
2026-05-04 15:14:39 +0900

    [level 2] Title: 타겟 넘버, Time: 5.63 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -3,19 +3,19 @@ class Solution {
     int answer = 0;
     public int solution(int[] numbers, int target) {
 
-        dfs(0, numbers.length, 0, target, numbers);
+        dfs(0, 0, target, numbers);
         return answer;
     }
     
-    public void dfs(int cur, int end, int result, int target, int[] numbers) {
-        if(cur == end) {
+    void dfs(int cur, int result, int target, int[] numbers) {
+        if(cur == numbers.length) {
             if(result == target) {
                 answer++;
             }
             return;
         }
         
-        dfs(cur + 1, end, result + numbers[cur], target, numbers);
-        dfs(cur + 1, end, result - numbers[cur], target, numbers);
+        dfs(cur + 1, result + numbers[cur], target, numbers);
+        dfs(cur + 1, result - numbers[cur], target, numbers);
     }
 }
