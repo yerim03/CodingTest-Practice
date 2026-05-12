41c32951c43d59f576551889f9496c3666ce2d6c
나예림 <103301583+yerim03@users.noreply.github.com>
2026-05-13 01:10:50 +0900

    [level 2] Title: 피로도, Time: 1.71 ms, Memory: 64.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,29 +1,26 @@
-import java.util.*;
-
+//최소 필요 피로도 - 탐험을 하기 위한 최소한의 피로도
+//소모 피로도 - 탐험 후 소모되는 피로도
+//유저가 탐험할 수 있는 최대 던전 수
 class Solution {
     
-    static boolean[] visited;
-    static int max = 0;
+    int answer = 0;
+    boolean[] visited;
     
     public int solution(int k, int[][] dungeons) {
-        
+
         visited = new boolean[dungeons.length];
         dfs(k, 0, dungeons);
-        
-        return max;
-        
+        return answer;
     }
-
-    public void dfs(int k, int cnt, int[][] dungeons) {
-        max = Math.max(max, cnt);
+    
+    public void dfs(int cur, int cnt, int[][] dungeons) {
         for(int i = 0; i < dungeons.length; i++) {
-            if(!visited[i] && dungeons[i][0] <= k) {
+            if(!visited[i] && cur >= dungeons[i][0]) {
                 visited[i] = true;
-                dfs(k - dungeons[i][1], cnt + 1, dungeons);
+                dfs(cur - dungeons[i][1], cnt + 1, dungeons);
                 visited[i] = false;
             }
         }
-        
-        
+        answer = Math.max(answer, cnt);
     }
 }
